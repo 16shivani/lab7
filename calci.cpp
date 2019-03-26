37e81cdf0a993e050ed5a8fd82ffead102ce10fd
shivani chauhan <shivanichauhan0116@gmail.com>
2019-03-26 10:36:48 +0530

    Defining main

--- calci.cpp
+++ calci.cpp
@@ -5,11 +5,56 @@ using namespace std
 
 class calci
 {
-	int x,y;
 	public:
+	int x,y,result;
+
 	int sum (x, y);
 	int subtract (x, y);
 	int multiply (x, y);
 	int division (x, y);
 
+
 }
+
+int main()
+{
+	int op;
+	cout<<"enter first value/n";
+	cin>>x;
+	cout<<"enter second value";
+	cin>>y;
+	cout<<"enter operation ( + - * / )";
+	cin>>op;
+
+	if(op==+)
+	{ 
+		sum(x, y);
+		break;
+	}
+	if(op==-)
+	{
+		subtract(x, y);
+		break;
+	}
+	if(op==*)
+	{
+		multiply(x, y);
+		break;
+	}
+	if(op==/)
+	{
+		division(x, y);
+		break;
+	}
+cout<<"result:"<<result;
+return 0;
+} 
+
+
+
+
+
+
+
+
+
