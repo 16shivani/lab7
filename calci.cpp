3f1628f63e89eab9f45f76b3544b233214bebfda
shivani chauhan <shivanichauhan0116@gmail.com>
2019-03-26 10:18:49 +0530

    Function Declaration

---
+++ calci.cpp
@@ -0,0 +1,15 @@
+code for calci
+
+#include <iostream>
+using namespace std
+
+class calci
+{
+	int x,y;
+	public:
+	int sum (x, y);
+	int subtract (x, y);
+	int multiply (x, y);
+	int division (x, y);
+
+}
